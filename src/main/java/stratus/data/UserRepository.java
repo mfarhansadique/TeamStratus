46ec1f460ea896e1bd0874a2cbf6726706c6087c
Rose <rose@example.com>
2019-07-25 10:25:28 +0100

    added Maven dependencies for Maps file

--- src/main/java/stratus/data/UserRepository.java
+++ src/main/java/stratus/data/UserRepository.java
@@ -1,6 +1,7 @@
 package stratus.data;
 
 import org.springframework.data.repository.CrudRepository;
+import stratus.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
