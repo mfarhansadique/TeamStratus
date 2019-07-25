c5e31774e42e5b8925964119fb15de6c136cfc89
Sheila <sheila@sheila.com>
2019-07-25 14:31:22 +0100

    SK - created SecurityConfig file

--- src/main/java/stratus/SecurityConfig.java
+++ src/main/java/stratus/SecurityConfig.java
@@ -2,17 +2,34 @@ package stratus;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.net.Authenticator;
 
 @Configuration
-//@EnableWebSecurity
-//EnableWebMvcSecurity
+@EnableWebSecurity
 
-public class SecurityConfig // extends WebSecurityConfigurationAdaptor
-{
-//see slides for sample code
-//@Override
-//protected void configure(HttpSecurity http) throws Exception {}
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Autowired
+    private GYWUserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        System.out.println("configuring aithorisation with UDS= " + userDetailsService.toString());
+
+//        http.httpBasic().and().authorizeRequests()
+//            .antMatchers....
+    }
+
 }
 
-
-   // BCryptPasswordEncider encoder = new BCryptPasswordEncider(); - scrambles passwords
