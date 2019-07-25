1ffea8ff19e072e1d19f1ad9c1db0cdb326ab17c
Dominika Malinowska <dominika.malinowska@sky.com>
2019-07-25 10:35:58 +0100

    DM - Creating user, admin and route objects & security comments

--- src/main/java/stratus/Config.java
+++ src/main/java/stratus/Config.java
@@ -4,7 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories("stratus")
+@EnableJpaRepositories("stratus.data")
 
 public class Config {
 }
+
+
+//security configuartions go here
+
+//Do we need MI to show business value??
