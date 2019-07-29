bc5cd9e7789b0401b4ef79fc0f26e0e294435601
Dominika Malinowska <dominika.malinowska@sky.com>
2019-07-29 14:42:28 +0100

    DM - Inserting new Route and Users again

--- src/main/java/stratus/Application.java
+++ src/main/java/stratus/Application.java
@@ -17,20 +17,6 @@ public class Application {
 
         SpringApplication.run(Application.class, args);
 
-//    public static void main(String[] args) {
-//        ApplicationContext ctx = new AnnotationConfigApplicationContext("stratus");
-//        UserDAO userDao = ctx.getBean(UserDAO.class);
-//
-//        ApplicationContext ctx2 = new AnnotationConfigApplicationContext("stratus");
-//        RouteDAO routeDao = ctx2.getBean(RouteDAO.class);
-//
-//        User dom = new User("Dominika", "Malinowska", "246 Priory Road", "Birmingham", "B28 0SU", "dom", "password", "do_ma1991@hotmail.com", "07904295900", 'A', null );
-//        userDao.save(dom);
-//
-//        System.out.println(userDao.findAll());
-//    }
-//}
-
         ApplicationContext ctx = new AnnotationConfigApplicationContext("stratus");
         UserDAO userDao = ctx.getBean(UserDAO.class);
         RouteDAO routeDao = ctx.getBean(RouteDAO.class);
