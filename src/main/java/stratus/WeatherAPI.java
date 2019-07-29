ec4f6eff41519d268095013effa9e137b5d13c5f
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-07-29 15:51:30 +0100

    got return for wetaher API, for present and next week

--- src/main/java/stratus/WeatherAPI.java
+++ src/main/java/stratus/WeatherAPI.java
@@ -1,7 +1,9 @@
 package stratus;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class WeatherAPI {
@@ -44,10 +46,53 @@ public class WeatherAPI {
         return getWeatherByLatLon(Double.toString(lat),Double.toString(lon));
     }
 
+    private static String[] outputWeatherNow(String string){
+        JSONObject myObjectData = new JSONObject(string);
+        Float temp= myObjectData.getJSONObject("currently").getFloat("temperature");
+
+        String summary=myObjectData.getJSONObject("currently").getString("icon");
+        String icon=myObjectData.getJSONObject("currently").getString("summary");
+        String temps=temp.toString();
+
+        String[] result=new String[]{temps,icon,summary};
+        return result;
+    }
+
+
+    private static String[] outputWeatherWeek(String string, int dayDiff){
+        //dayDiff is the amount of days of difference between today and the day of interest plus 1. So if it is Monday and we want Tuesday we need to enter 2
+        JSONObject myObjectData = new JSONObject(string);
+        String[] result=new String[3];
+
+        if((0<=dayDiff)&&(dayDiff<=6)){
+            Float temp =myObjectData.getJSONObject("daily").getJSONArray("data").getJSONObject(dayDiff).getFloat("temperature");
+            result[0]=temp.toString();
+            result[1]=myObjectData.getJSONObject("daily").getJSONArray("data").getJSONObject(dayDiff).getString("icon");
+             result[2]=myObjectData.getJSONObject("daily").getJSONArray("data").getJSONObject(dayDiff).getString("summary");
+
+        return result;
+        }
+        return result;
+    }
+
+    private static String[] outputWeatherPast(String lat, String lon, String time){
+        // have to create a new function that does everything since different http request
+        apiCaller.getRapidApiResponse("https://dark-sky.p.rapidapi.com/"+ lat +","+ lon +"?lang=en&units=auto");
+        JSONObject myObjectData = new JSONObject(string);
+        Float temp= myObjectData.getJSONObject("daily").getJSONArray("data").getJSONObject(0).getFloat("temperature");
+
+        String summary=myObjectData.getJSONObject("daily").getJSONArray("data").getJSONObject(0).getString("icon");
+        String icon=myObjectData.getJSONObject("daily").getJSONArray("data").getJSONObject(0).getString("summary");
+        String temps=temp.toString();
+
+        String[] result=new String[]{temps,icon,summary};
+        return result;
+    }
 
     public static void main(String[] args) {
         scn = new Scanner(System.in);
         System.out.println(PrettyJSON.print(getWeatherByLatLon("51.89381100000001","-0.2873046")));
+        //System.out.println(Arrays.toString(outputWeatherNow(getWeatherByLatLon("51.89381100000001","-0.2873046"))));
 
     }
 }
