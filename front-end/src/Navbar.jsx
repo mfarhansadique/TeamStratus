19a5fc835085de25f41799d9559c67213fae3811
Dominika Malinowska <dominika.malinowska@sky.com>
2019-07-30 16:45:38 +0100

    React

---
+++ front-end/src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Navbar = () => {
+                 return(
+                    <nav class="navbar navbar-dark bg-dark">
+                        <span class="navbar-brand mb-0 h1">GetYourWay</span>
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="register">Register</button>
+                    </nav>
+                 )};
+                export default Navbar;
