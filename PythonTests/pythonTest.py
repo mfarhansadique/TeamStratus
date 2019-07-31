f473d25d37b20fb9398bc6d4cc430953dbe95da5
farhan <farhan1@live.co.uk>
2019-07-31 16:30:06 +0100

    began writing python test scripts

---
+++ PythonTests/pythonTest.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import os
+import time
+
+
+
+class testone():
+    driver = None
+
+    def opening_browser(self):
+        driverLocation = "./SeleniumDriver/chromedriver"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        self.driver = webdriver.Chrome(driverLocation)
+        self.driver.get("http://google.com")
+        time.sleep(1)
+        inputElement = self.driver.find_element_by_xpath('//*[@id="root"]/div/nav/a[3]/button').click()
+        self.driver.maximize_window()
+
+
+
+
+    def input_into_searchbar(self):
+     inputElement = self.driver.find_element_by_name('firstname')
+
+     inputElement.send_keys("6666")
+     inputElement = self.driver.find_element_by_name('lastname')
+     inputElement.send_keys("DaBuilder")
+     inputElement = self.driver.find_element_by_name('username')
+     inputElement.send_keys("canwefixit4")
+     inputElement = self.driver.find_element_by_name('email')
+     inputElement.send_keys("bob4@hotmail.com")
+     inputElement = self.driver.find_element_by_name('password')
+     inputElement.send_keys("password")
+     inputElement = self.driver.find_element_by_name('confirmpassword')
+     inputElement.send_keys("password")
+     inputElement = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/div/form/input[7]').click()
+     time.sleep(5)
+     self.driver.close()
+
+
+gc = testone()
+gc.opening_browser()
+# gc.input_into_searchbar()
