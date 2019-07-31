25174825f8c40e398d69d049f200b17a8916b40c
Nasim Ali <nasim133@icloud.com>
2019-07-31 14:48:46 +0100

    Created a route to test

--- src/main/java/stratus/web/ApiController.java
+++ src/main/java/stratus/web/ApiController.java
@@ -2,11 +2,14 @@ package stratus.web;
 
 import com.google.gson.Gson;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import stratus.API.AirportInformation;
 import stratus.API.HttpApiResponse;
 import stratus.API.AmadeusFlightsApi;
 import stratus.API.WeatherAPI;
+import stratus.DAO.Route;
+import stratus.DAO.RouteDAO;
 
 
 import java.util.List;
@@ -14,7 +17,8 @@ import java.util.List;
 @RestController
 public class ApiController {
 
-
+@Autowired
+RouteDAO route;
 
 
  @GetMapping("/getairportcode/{location}")
@@ -27,6 +31,8 @@ public class ApiController {
      return json;
  }
 
+ //ignore
+
  @GetMapping("/getflight/{destinationcode}/{arrivalcode}/{destinationdate}")
  @ResponseBody
  public String getFlight(@PathVariable("destinationcode") String destinationcode, @PathVariable("arrivalcode") String arrivalcode, @PathVariable("destinationdate") String destinationdate){
@@ -35,6 +41,8 @@ public class ApiController {
   String arrivalWeather = WeatherAPI.getWeatherByAirportCode(arrivalcode);
   String createWeatherJson = "{\"destinationWeather\":"+destinationWeather + ",\"arrivalWeather\":"+arrivalWeather+"}";
   String json = flights.toString();
+  route.save(new Route(json,json,json,json,json,json,json,null));
+
   //json =  json + "," + createWeatherJson ;
   return json;
  }
